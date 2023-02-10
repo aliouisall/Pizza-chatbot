0bacda17b84a932df6e1bb9ae7dab53ff5d7f43a
aliouisall <aliouibnibrahim@gmail.com>
2023-02-10 12:19:47 +0100

    Setting fullfilment text

--- src/main/java/com/example/demo/ChatbotController.java
+++ src/main/java/com/example/demo/ChatbotController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +12,16 @@ public class ChatbotController {
     @PostMapping("/webhook")
     public WebhookResponse webhook(@RequestBody WebhookRequest data){
         log.info("{}", data);
-        WebhookResponse webhRes = new WebhookResponse();
-        webhRes.setFullfilmentText("Test");
-        log.info("{}", webhRes);
-        return webhRes;
+        WebhookResponse webHoRes = new WebhookResponse();
+        log.info("{}", webHoRes);
+        log.info("{}", data.getQueryResult().getParameters());
+        var genre = (String)data.getQueryResult().getParameters().get("Genre");
+        if (genre == null || genre.isEmpty()){
+            webHoRes.setFullfilmentText("Quel genre de film ?");
+        }
+        else{
+            webHoRes.setFullfilmentText("Vous voulez voir un film de " + genre + ". Bien.");
+        }
+        return webHoRes;
     }
 }
--- src/main/java/com/example/demo/WebhookRequest.java
+++ src/main/java/com/example/demo/WebhookRequest.java
@@ -2,13 +2,17 @@ package com.example.demo;
 
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 public class WebhookRequest {
     private String responseId;
+    private QueryResult queryResult;
 
     @Data
     public class QueryResult{
         private String queryText;
         private String action;
+        private Map<String, Object> parameters;
     }
 }
