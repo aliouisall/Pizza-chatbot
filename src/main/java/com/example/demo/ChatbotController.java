871fc82b0f1b79228b7bd9e314f595aefaffb1d8
aliouisall <aliouibnibrahim@gmail.com>
2023-02-10 15:57:43 +0100

    Setting platform

--- src/main/java/com/example/demo/ChatbotController.java
+++ src/main/java/com/example/demo/ChatbotController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 public class ChatbotController {
@@ -17,11 +19,16 @@ public class ChatbotController {
         log.info("{}", data.getQueryResult().getParameters());
         var genre = (String)data.getQueryResult().getParameters().get("Genre");
         if (genre == null || genre.isEmpty()){
-            webHoRes.setFullfilmentText("Quel genre de film ? ");
+            webHoRes.setFullfilmentText("Quel genre de film ?");
         }
         else{
             webHoRes.setFullfilmentText("Vous voulez voir un film de " + genre + ". Bien.");
         }
+        webHoRes.setFulfillmentMessages(List.of(new WebhookResponse.FulfillmentMessage().setPlatform("ACTIONS_ON_GOOGLE")));
         return webHoRes;
     }
 }
+
+// class ffmes
+// class sugg contient list de suggestion ayant title comme propriété; même chose pour simple response
+//
--- src/main/java/com/example/demo/WebhookResponse.java
+++ src/main/java/com/example/demo/WebhookResponse.java
@@ -2,7 +2,27 @@ package com.example.demo;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @Data
 public class WebhookResponse {
     private String fullfilmentText;
+    private List<FulfillmentMessage> FulfillmentMessages;
+
+    @Data
+    public static class FulfillmentMessage{
+        private String platform;
+        private List<Suggestion> Suggestions;
+        @Data
+        public static class Suggestion{
+            private String title;
+        }
+        private List<SimpleResponse> SimpleResponses;
+        @Data
+        public static class SimpleResponse{
+            private String textToSpeech;
+        }
+    }
 }
