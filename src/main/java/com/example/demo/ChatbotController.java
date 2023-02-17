1e22833a39339e13c957f5369d3d560a9f58473c
aliouisall <aliouibnibrahim@gmail.com>
2023-02-17 15:37:22 +0100

    Looping over movies of selected genre

--- src/main/java/com/example/demo/ChatbotController.java
+++ src/main/java/com/example/demo/ChatbotController.java
@@ -1,26 +1,28 @@
 package com.example.demo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 public class ChatbotController {
     @PostMapping("/webhook")
-    public WebhookResponse webhook(@RequestBody WebhookRequest data) throws JsonProcessingException {
+    public WebhookResponse webhook(@RequestBody WebhookRequest data){
         RestTemplate restTemplate = new RestTemplate();
         String tmdbResourceUrl = "https://api.themoviedb.org/3/search/movie?api_key=7996ca1b1e575167242fa0d3ea6f5ef1";
-        MovieDBResponse response = restTemplate.getForObject(tmdbResourceUrl + "&query=Jack+Reacher&language=fr", MovieDBResponse.class);
-        log.info("{}", response.getResults().get(0).getTitle());
+        Map <String, List<String>> movies = new HashMap<>();
+        movies.put("Action", List.of("Aquaman", "Uncharted"));
+        movies.put("Comédie", List.of("Ted", "Paddington"));
+        log.info("{}", movies.keySet());
         log.info("{}", data);
         WebhookResponse webHoRes = new WebhookResponse();
         log.info("{}", webHoRes);
@@ -47,6 +49,17 @@ public class ChatbotController {
             );
         }
         else {
+            log.info("{}", movies.get(genre));
+            List<WebhookResponse.FulfillmentMessage.Item> items = new ArrayList<>();
+            for(String movie:movies.get(genre)){
+                MovieDBResponse genreSelectedResponse = restTemplate.getForObject(tmdbResourceUrl + "&query=" + movie + "&language=fr", MovieDBResponse.class);
+                WebhookResponse.FulfillmentMessage.Item item = new WebhookResponse.FulfillmentMessage.Item()
+                        .setTitle(genreSelectedResponse.getResults().get(0).getTitle())
+                        .setDescription(genreSelectedResponse.getResults().get(0).getOverview())
+                        .setInfo(new WebhookResponse.FulfillmentMessage.Info().setKey(genreSelectedResponse.getResults().get(0).getId().toString()))
+                        .setImage(new WebhookResponse.FulfillmentMessage.Image().setImageUri("https://image.tmdb.org/t/p/w500" + genreSelectedResponse.getResults().get(0).getPosterPath()));
+                items.add(item);
+            }
             webHoRes.setFulfillmentMessages(List.of(
                     new WebhookResponse.FulfillmentMessage()
                             .setPlatform("ACTIONS_ON_GOOGLE")
@@ -55,24 +68,11 @@ public class ChatbotController {
                     ))),
                     new WebhookResponse.FulfillmentMessage()
                             .setPlatform("ACTIONS_ON_GOOGLE")
-                            .setCarouselSelect(new WebhookResponse.FulfillmentMessage.CarouselSelect().setItems(List.of(
-                                    new WebhookResponse.FulfillmentMessage.Item()
-                                            .setTitle(response.getResults().get(0).getTitle())
-                                            .setDescription(response.getResults().get(0).getOverview())
-                                            .setInfo(new WebhookResponse.FulfillmentMessage.Info().setKey(response.getResults().get(0).getId().toString()))
-                                            .setImage(new WebhookResponse.FulfillmentMessage.Image().setImageUri("https://image.tmdb.org/t/p/w500" + response.getResults().get(0).getPosterPath())),
-                                    new WebhookResponse.FulfillmentMessage.Item()
-                                            .setTitle(response.getResults().get(1).getTitle())
-                                            .setDescription(response.getResults().get(1).getOverview())
-                                            .setInfo(new WebhookResponse.FulfillmentMessage.Info().setKey(response.getResults().get(1).getId().toString()))
-                                            .setImage(new WebhookResponse.FulfillmentMessage.Image().setImageUri("https://image.tmdb.org/t/p/w500" + response.getResults().get(1).getPosterPath()))
-                    )))
+                            .setCarouselSelect(new WebhookResponse.FulfillmentMessage.CarouselSelect().setItems(items
+                    ))
             ));
         }
         log.info("{}", data.getQueryResult().getAction());
         return webHoRes;
     }
 }
-
-// après choix genre -> afficher 3 images de film sous forme de carrousel
-// placer bloc set items dans le else après test
